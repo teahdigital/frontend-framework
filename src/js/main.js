e3c8e0b8c71eed971c9ac77122bfaeaf039031d3
Víctor Araya J <vic9511@hotmail.com>
2015-08-04 15:07:56 -0600

    modified base js main

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,43 @@
 	"use strict";
 	var App = {
 		$body: null,
+		$main: null,
+		wh: null,
+		ww: null,
 
 		init:function(){
 			this._construct();
 			this.bindEvents();
-
+			
+			console.log("ready");
 		},
 
 		// custom functions
 		_construct:function(){
 			this.$body = $("body");
+			this.$main = $("#main");
+			this.wh = $(window).height();
+			this.ww = $(window).width();
 
+			this.resize();
 		},
 
 		// custom functions
+		styles:function(){
+			this.$main.css({ height: this.wh }); 
+		},
 
 		//events section
 		bindEvents:function(){
 			$(window).resize( this.resize );
 
 		}, 
-		resize:function(){
 
+		resize:function(){
+			this.wh = $(window).height();
+			this.ww = $(window).width();
+
+			this.styles();
 		}
 	};
 
